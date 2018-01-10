ac340312b839bd8e91b40ec012e97ba2df2f279b
Joey Newfield <joey.newfield@gmail.com>
2018-01-10 00:03:35 -0800

    Buttons have been added

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Joey Newfield on 1/9/18.
+//  Copyright © 2018 Joey Newfield. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var recordButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+   
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
